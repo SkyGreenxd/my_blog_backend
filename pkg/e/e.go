b62cea2f13bfc8f174e8e3041d4e0977f78a5dab
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2025-08-09 22:23:44 +0300

    update errors

--- pkg/e/e.go
+++ pkg/e/e.go
@@ -7,9 +7,29 @@ import (
 )
 
 var (
-	ErrUserNotFound      = errors.New("user not found")
-	ErrUserDublicate     = errors.New("user with such email or username already exists")
-	ErrCategoryDublicate = errors.New("category with such name already exists")
+	// users
+	ErrUserNotFound  = errors.New("user not found")
+	ErrUserDuplicate = errors.New("user with such email or username already exists")
+	// username
+	ErrUsernameEmpty        = errors.New("username is empty")
+	ErrUsernameTooShort     = errors.New("username is too short")
+	ErrUsernameTooLong      = errors.New("username is too long")
+	ErrUsernameInvalidChars = errors.New("username contains invalid characters")
+	ErrUsernameHasSpaces    = errors.New("username contains spaces")
+	// role
+	ErrInvalidRole = errors.New("invalid role")
+	// email
+	ErrEmailTooLong       = errors.New("email is too long")
+	ErrEmailInvalidFormat = errors.New("email format is invalid")
+	ErrEmailHasSpaces     = errors.New("email contains spaces")
+	ErrEmailTooShort      = errors.New("email is too short")
+	// password
+	ErrPasswordTooShort  = errors.New("password hash is too short")
+	ErrPasswordTooLong   = errors.New("password hash is too long")
+	ErrPasswordHasSpaces = errors.New("password hash contains spaces")
+
+	// categories
+	ErrCategoryDuplicate = errors.New("category with such name already exists")
 	ErrCategoryNotFound  = errors.New("category not found")
 	ErrCategoryNameEmpty = errors.New("category name is empty")
 	ErrCategoryNameLong  = errors.New("category name is too long")
