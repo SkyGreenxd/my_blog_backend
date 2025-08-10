a1f8bb1b7aa9e80226ec0761615c70820329c938
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2025-08-11 00:34:00 +0300

    Implement data validation

--- internal/domain/article.go
+++ internal/domain/article.go
@@ -1,7 +1,10 @@
 package domain
 
 import (
+	"my_blog_backend/pkg/e"
+	"strings"
 	"time"
+	"unicode/utf8"
 )
 
 type Article struct {
@@ -12,4 +15,53 @@ type Article struct {
 	CategoryID uint
 	CreatedAt  time.Time
 	UpdatedAt  time.Time
+}
+
+func (a *Article) Validate() error {
+	if err := validateTitle(a.Title); err != nil {
+		return err
+	}
+
+	if err := validateContent(a.Content); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func validateTitle(title string) error {
+	title = strings.TrimSpace(title)
+	length := utf8.RuneCountInString(title)
+
+	if length < 3 {
+		return e.ErrTitleTooShort
+	}
+
+	if length > 100 {
+		return e.ErrTitleTooLong
+	}
+
+	if strings.Contains(title, "<") || strings.Contains(title, ">") {
+		return e.ErrTitleHasHTML
+	}
+
+	return nil
+}
+
+func validateContent(content string) error {
+	content = strings.TrimSpace(content)
+	length := utf8.RuneCountInString(content)
+
+	if length < 10 {
+		return e.ErrContentTooShort
+	}
+
+	if length > 16000 {
+		return e.ErrContentTooLong
+	}
+
+	if strings.Contains(content, "<script") {
+		return e.ErrContentHasScript
+	}
+	return nil
 }
