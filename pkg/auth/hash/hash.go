f53d306aa732dc7ec87a7cc4199e9435893301be
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2025-08-12 20:26:14 +0300

    add new error handling for Compare func

--- pkg/auth/hash/hash.go
+++ pkg/auth/hash/hash.go
@@ -3,6 +3,7 @@ package hash
 import (
 	"fmt"
 	"golang.org/x/crypto/bcrypt"
+	"my_blog_backend/pkg/e"
 )
 
 type BcryptHashManager struct {
@@ -26,5 +27,30 @@ func (manager *BcryptHashManager) HashPassword(password string) (string, error) 
 }
 
 func (manager *BcryptHashManager) Compare(password string, hash string) error {
-	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	if err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password)); err != nil {
+		if err == bcrypt.ErrMismatchedHashAndPassword {
+			return e.ErrMismatchedHashAndPassword
+		}
+		return fmt.Errorf("error comparing password: %w", err)
+	}
+
+	return nil
 }
+
+//func HashToken(token string) string {
+//	// 1. Создаем новый объект хешера SHA-256.
+//	hasher := sha256.New()
+//
+//	// 2. Записываем в хешер байты нашего токена.
+//	// Важно передавать байты, а не просто строку.
+//	hasher.Write([]byte(token))
+//
+//	// 3. Вычисляем хеш. `Sum(nil)` возвращает результат в виде среза байт.
+//	hashBytes := hasher.Sum(nil)
+//
+//	// 4. Кодируем срез байт в шестнадцатеричную строку.
+//	// [32]byte -> "6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b"
+//	hashString := hex.EncodeToString(hashBytes)
+//
+//	return hashString
+//}
