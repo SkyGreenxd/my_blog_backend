8303f62bb9d5164b435a99f1027809ee80806751
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2025-08-12 20:24:21 +0300

    add session struct and constructor func

---
+++ internal/domain/session.go
@@ -0,0 +1,45 @@
+package domain
+
+import (
+	"github.com/google/uuid"
+	"my_blog_backend/pkg/e"
+	"time"
+)
+
+type Session struct {
+	Id               uuid.UUID
+	UserId           uint
+	RefreshTokenHash string
+	IsRevoked        bool
+	CreatedAt        time.Time
+	ExpiresAt        time.Time
+}
+
+func NewSession(userID uint, refreshTokenHash string, expiresAt time.Time) *Session {
+	return &Session{
+		Id:               uuid.New(),
+		UserId:           userID,
+		RefreshTokenHash: refreshTokenHash,
+		IsRevoked:        false,
+		CreatedAt:        time.Now().UTC(),
+		ExpiresAt:        expiresAt,
+	}
+}
+
+func (s *Session) ValidateState() error {
+	// Отозвана ли сессия?
+	if s.IsRevoked {
+		return e.ErrSessionRevoked
+	}
+
+	// Истек ли срок действия?
+	if s.ExpiresAt.Before(time.Now().UTC()) {
+		return e.ErrSessionExpired
+	}
+
+	return nil
+}
+
+func (s *Session) Revoke() {
+	s.IsRevoked = true
+}
