b0093c3d6e321fd94c2f7408b7ccf80e570a8f69
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2025-08-14 03:48:42 +0300

    del logs and add errors wrap

--- internal/repository/postgres/helpers.go
+++ internal/repository/postgres/helpers.go
@@ -3,32 +3,29 @@ package postgres
 import (
 	"errors"
 	"gorm.io/gorm"
-	"log"
 	"my_blog_backend/pkg/e"
 )
 
-func checkChangeQueryResult(result *gorm.DB, op, message string, notFound error) error {
+func checkChangeQueryResult(result *gorm.DB, op string, notFound error) error {
 	if err := result.Error; err != nil {
-		return e.WrapDBError(op, err)
+		return e.Wrap(op, err)
 	}
 
 	if result.RowsAffected == 0 {
-		return notFound
+		return e.Wrap(op, notFound)
 	}
 
-	log.Printf("%s: %s", op, message)
 	return nil
 }
 
-func checkGetQueryResult(result *gorm.DB, op, message string, notFound error) error {
+func checkGetQueryResult(result *gorm.DB, notFound error) error {
 	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
 		return notFound
 	}
 
 	if err := result.Error; err != nil {
-		return e.WrapDBError(op, err)
+		return err
 	}
 
-	log.Printf("%s: %s", op, message)
 	return nil
 }
