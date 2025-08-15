8c3e406eb698a4284da798457e2f3488bb7bb7fc
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2025-08-15 19:28:14 +0300

    add sample

--- internal/usecase/article_uc.go
+++ internal/usecase/article_uc.go
@@ -1,1 +1,14 @@
 package usecase
+
+import (
+	"my_blog_backend/internal/repository"
+)
+
+// TODO: Реализовать сервис, перенести логику валидации сюда
+type ArticleService struct {
+	articleRepo repository.ArticleRepository
+}
+
+func NewArticleService(a repository.ArticleRepository) *ArticleService {
+	return &ArticleService{articleRepo: a}
+}
--- internal/usecase/category_uc.go
+++ internal/usecase/category_uc.go
@@ -1,1 +1,12 @@
 package usecase
+
+import "my_blog_backend/internal/repository"
+
+// TODO: Реализовать сервис, перенести логику валидации сюда
+type CategoryService struct {
+	categoryRepo repository.CategoryRepository
+}
+
+func NewCategoryService(c repository.CategoryRepository) *CategoryService {
+	return &CategoryService{categoryRepo: c}
+}
