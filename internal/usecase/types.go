0cd581b9fbd9cbaa3240875843e434565e78a80f
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2025-08-15 19:08:33 +0300

    del json tags

--- internal/usecase/types.go
+++ internal/usecase/types.go
@@ -6,29 +6,29 @@ import (
 )
 
 type CreateUserReq struct {
-	Username string `json:"username"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	Username string
+	Email    string
+	Password string
 }
 
 type LoginUserReq struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	Email    string
+	Password string
 }
 
 type UserRes struct {
-	Username string      `json:"username"`
-	Email    string      `json:"email"`
-	Role     domain.Role `json:"role"`
+	Username string
+	Email    string
+	Role     domain.Role
 }
 
 type LoginUserRes struct {
-	SessionID             string    `json:"session_id"`
-	AccessToken           string    `json:"access_token"`
-	RefreshToken          string    `json:"refresh_token"`
-	AccessTokenExpiresAt  time.Time `json:"access_token_expires_at"`
-	RefreshTokenExpiresAt time.Time `json:"refresh_token_expires_at"`
-	User                  UserRes   `json:"user"`
+	SessionID             string
+	AccessToken           string
+	RefreshToken          string
+	AccessTokenExpiresAt  time.Time
+	RefreshTokenExpiresAt time.Time
+	User                  UserRes
 }
 
 func (u *LoginUserReq) Validate() error {
