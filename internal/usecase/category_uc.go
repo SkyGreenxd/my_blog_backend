a0401c0dd394ea7706bdf819b0015ab27429fc81
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2025-08-16 05:00:21 +0300

    realize category.go

--- internal/usecase/category_uc.go
+++ internal/usecase/category_uc.go
@@ -1,12 +1,103 @@
 package usecase
 
-import "my_blog_backend/internal/repository"
+import (
+	"context"
+	"errors"
+	"my_blog_backend/internal/domain"
+	"my_blog_backend/internal/repository"
+	"my_blog_backend/pkg/e"
+)
 
-// TODO: Реализовать сервис, перенести логику валидации сюда
 type CategoryService struct {
 	categoryRepo repository.CategoryRepository
 }
 
 func NewCategoryService(c repository.CategoryRepository) *CategoryService {
 	return &CategoryService{categoryRepo: c}
+}
+
+func (s *CategoryService) Create(ctx context.Context, createCategoryReq *CreateCategoryReq) (string, error) {
+	const op = "CategoryService.Create"
+
+	if createCategoryReq.UserRole != domain.RoleAdmin {
+		return "", e.Wrap(op, e.ErrPermissionDenied)
+	}
+
+	newCategory := domain.NewCategory(createCategoryReq.CategoryName)
+
+	categoryEntity, err := s.categoryRepo.Create(ctx, newCategory)
+	if err != nil {
+		if errors.Is(err, e.ErrCategoryInUse) {
+			return "", e.Wrap(op, err)
+		}
+
+		return "", e.Wrap(op, e.ErrInternalServer)
+	}
+
+	return categoryEntity.Name, nil
+}
+
+func (s *CategoryService) GetAll(ctx context.Context) ([]string, error) {
+	const op = "CategoryService.GetAll"
+
+	categories, err := s.categoryRepo.ListAll(ctx)
+	if err != nil {
+		return nil, e.Wrap(op, e.ErrInternalServer)
+	}
+
+	result := make([]string, len(categories))
+	for i, category := range categories {
+		result[i] = category.Name
+	}
+
+	return result, nil
+}
+
+func (s *CategoryService) Update(ctx context.Context, updateCategoryReq *UpdateCategoryReq) error {
+	const op = "CategoryService.Update"
+
+	if updateCategoryReq.UserRole != domain.RoleAdmin {
+		return e.Wrap(op, e.ErrPermissionDenied)
+	}
+
+	category, err := s.categoryRepo.GetByID(ctx, updateCategoryReq.CategoryId)
+	if err != nil {
+		if errors.Is(err, e.ErrCategoryNotFound) {
+			return e.Wrap(op, e.ErrCategoryNotFound)
+		}
+
+		return e.Wrap(op, e.ErrInternalServer)
+	}
+
+	if err := category.ChangeName(updateCategoryReq.NewCategoryName); err != nil {
+		return e.Wrap(op, err)
+	}
+
+	if err := s.categoryRepo.Update(ctx, category); err != nil {
+		return e.Wrap(op, e.ErrInternalServer)
+	}
+
+	return nil
+}
+
+func (s *CategoryService) Delete(ctx context.Context, deleteCategoryReq *DeleteCategoryReq) error {
+	const op = "CategoryService.Delete"
+
+	if deleteCategoryReq.UserRole != domain.RoleAdmin {
+		return e.Wrap(op, e.ErrPermissionDenied)
+	}
+
+	if err := s.categoryRepo.Delete(ctx, deleteCategoryReq.CategoryId); err != nil {
+		if errors.Is(err, e.ErrCategoryNotFound) {
+			return e.Wrap(op, e.ErrCategoryNotFound)
+		}
+
+		if errors.Is(err, e.ErrCategoryInUse) {
+			return e.Wrap(op, e.ErrCategoryInUse)
+		}
+
+		return e.Wrap(op, e.ErrInternalServer)
+	}
+
+	return nil
 }
