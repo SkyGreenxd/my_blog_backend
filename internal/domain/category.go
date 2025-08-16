fb5d3b73a891454b788dea90a73e57d109b202af
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2025-08-16 05:01:24 +0300

    change validate func, add constructor func

--- internal/domain/category.go
+++ internal/domain/category.go
@@ -2,9 +2,7 @@ package domain
 
 import (
 	"my_blog_backend/pkg/e"
-	"strings"
 	"time"
-	"unicode/utf8"
 )
 
 type Category struct {
@@ -18,25 +16,40 @@ func (*Category) TableName() string {
 	return "categories"
 }
 
-func (c *Category) Validate() error {
-	if err := ValidateCategoryName(c.Name); err != nil {
-		return err
+func NewCategory(name string) *Category {
+	return &Category{
+		Name: name,
+	}
+}
+
+func (c *Category) ChangeName(newName string) error {
+	if c.Name == newName {
+		return e.ErrCategoryIsExists
 	}
 
+	c.Name = newName
 	return nil
 }
 
-func ValidateCategoryName(name string) error {
-	name = strings.TrimSpace(name)
-	length := utf8.RuneCountInString(name)
-
-	if length < 2 {
-		return e.ErrCategoryTooShort
-	}
-
-	if length > 128 {
-		return e.ErrCategoryTooLong
-	}
-
-	return nil
-}
+//func (c *Category) Validate() error {
+//	if err := ValidateCategoryName(c.Name); err != nil {
+//		return err
+//	}
+//
+//	return nil
+//}
+//
+//func ValidateCategoryName(name string) error {
+//	name = strings.TrimSpace(name)
+//	length := utf8.RuneCountInString(name)
+//
+//	if length < 2 {
+//		return e.ErrCategoryTooShort
+//	}
+//
+//	if length > 128 {
+//		return e.ErrCategoryTooLong
+//	}
+//
+//	return nil
+//}
