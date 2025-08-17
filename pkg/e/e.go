74aa347eea9b4b44e68f50c8c3e868849626ff2c
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2025-08-18 02:35:47 +0300

    add new error

--- pkg/e/e.go
+++ pkg/e/e.go
@@ -58,14 +58,13 @@ var (
 	ErrRefreshTokenInvalid       = errors.New("refresh token is invalid")
 
 	// Общие ошибки
-	ErrInvalidEmail     = errors.New("invalid email")
-	ErrInvalidPassword  = errors.New("invalid password")
-	ErrInvalidId        = errors.New("invalid id")
-	ErrPermissionDenied = errors.New("permission denied")
-	ErrUnauthorized     = errors.New("unauthorized")
-	ErrParseFailed      = errors.New("parse failed")
-	ErrTokenInvalid     = errors.New("token is invalid")
-	ErrInternalServer   = errors.New("internal server error")
+	ErrInvalidEmail       = errors.New("invalid email")
+	ErrPermissionDenied   = errors.New("permission denied")
+	ErrUnauthorized       = errors.New("unauthorized")
+	ErrParseFailed        = errors.New("parse failed")
+	ErrTokenInvalid       = errors.New("token is invalid")
+	ErrInternalServer     = errors.New("internal server error")
+	ErrInvalidCredentials = errors.New("invalid credentials")
 )
 
 func Wrap(msg string, err error) error {
