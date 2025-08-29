3f625f3b0ea7df6049d7b3a97c15ae6d213036e0
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2025-08-30 00:41:03 +0300

    change errors

--- pkg/e/e.go
+++ pkg/e/e.go
@@ -15,9 +15,6 @@ var (
 	ErrUserAlreadyAdmin    = errors.New("user is already admin")
 	ErrEmailIsSame         = errors.New("email is same")
 	// username
-	ErrUsernameEmpty        = errors.New("username is empty")
-	ErrUsernameTooShort     = errors.New("username is too short")
-	ErrUsernameTooLong      = errors.New("username is too long")
 	ErrUsernameInvalidChars = errors.New("username contains invalid characters")
 	ErrUsernameHasSpaces    = errors.New("username contains spaces")
 	ErrUsernameIsExists     = errors.New("the user's username already exists.")
@@ -25,31 +22,27 @@ var (
 	// role
 	ErrInvalidRole = errors.New("invalid role")
 	// email
-	ErrEmailTooLong       = errors.New("email is too long")
 	ErrEmailInvalidFormat = errors.New("email format is invalid")
 	ErrEmailHasSpaces     = errors.New("email contains spaces")
-	ErrEmailTooShort      = errors.New("email is too short")
 	// password
-	ErrPasswordTooShort  = errors.New("password is too short")
-	ErrPasswordTooLong   = errors.New("password is too long")
 	ErrPasswordHasSpaces = errors.New("password contains spaces")
 
 	// categories
 	ErrCategoryIsExists     = errors.New("category with such name already exists")
 	ErrCategorySlugIsExists = errors.New("category slug already exists")
 	ErrCategoryNotFound     = errors.New("category not found")
-	ErrCategoryTooShort     = errors.New("category name is empty")
-	ErrCategoryTooLong      = errors.New("category name is too long")
 	ErrCategoryInUse        = errors.New("category is already in use")
 
 	// articles
-	ErrTitleTooShort    = errors.New("title is too short")
-	ErrTitleTooLong     = errors.New("title is too long")
-	ErrTitleHasHTML     = errors.New("title has html")
-	ErrContentTooShort  = errors.New("content is too short")
-	ErrContentTooLong   = errors.New("content is too long")
-	ErrContentHasScript = errors.New("content has script")
-	ErrArticleNotFound  = errors.New("article not found")
+	ErrTitleHasHTML            = errors.New("title has html")
+	ErrContentHasScript        = errors.New("content has script")
+	ErrArticleNotFound         = errors.New("article not found")
+	ErrArticleNameIsExists     = errors.New("the name of the article is not changed")
+	ErrArticleContentIsExists  = errors.New("the content of the article is not changed")
+	ErrArticleCategoryIsExists = errors.New("the category of the article is not changed")
+	ErrArticleDataIsInvalid    = errors.New("title or content of the article is invalid")
+	ErrUserNotAuthor           = errors.New("user is not author")
+	ErrArticleDuplicate        = errors.New("article is duplicate")
 
 	ErrMismatchedHashAndPassword = errors.New("password does not match hash")
 
@@ -61,7 +54,6 @@ var (
 	ErrRefreshTokenInvalid       = errors.New("refresh token is invalid")
 
 	// Общие ошибки
-	ErrInvalidEmail       = errors.New("invalid email")
 	ErrPermissionDenied   = errors.New("permission denied")
 	ErrUnauthorized       = errors.New("unauthorized")
 	ErrParseFailed        = errors.New("parse failed")
