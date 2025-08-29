87eb8bae553a1da69b8768064d8117757a3c186e
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2025-08-30 00:37:41 +0300

    add validation func

--- internal/domain/article.go
+++ internal/domain/article.go
@@ -1,6 +1,8 @@
 package domain
 
 import (
+	"my_blog_backend/pkg/e"
+	"strings"
 	"time"
 )
 
@@ -25,52 +27,65 @@ func NewArticle(title, content string, authorId, CategoryId uint) *Article {
 	}
 }
 
-// TODO: Перенести это в validators
-//func (a *Article) Validate() error {
-//	if err := ValidateTitle(a.Title); err != nil {
-//		return err
-//	}
-//
-//	if err := ValidateContent(a.Content); err != nil {
-//		return err
-//	}
-//
-//	return nil
-//}
-//
-//func ValidateTitle(title string) error {
-//	title = strings.TrimSpace(title)
-//	length := utf8.RuneCountInString(title)
-//
-//	if length < 3 {
-//		return e.ErrTitleTooShort
-//	}
-//
-//	if length > 100 {
-//		return e.ErrTitleTooLong
-//	}
-//
-//	if strings.Contains(title, "<") || strings.Contains(title, ">") {
-//		return e.ErrTitleHasHTML
-//	}
-//
-//	return nil
-//}
-//
-//func ValidateContent(content string) error {
-//	content = strings.TrimSpace(content)
-//	length := utf8.RuneCountInString(content)
-//
-//	if length < 10 {
-//		return e.ErrContentTooShort
-//	}
-//
-//	if length > 16000 {
-//		return e.ErrContentTooLong
-//	}
-//
-//	if strings.Contains(content, "<script") {
-//		return e.ErrContentHasScript
-//	}
-//	return nil
-//}
+func (a *Article) Validate() error {
+	if err := ValidateTitle(a.Title); err != nil {
+		return err
+	}
+
+	if err := ValidateContent(a.Content); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func ValidateTitle(title string) error {
+	if strings.Contains(title, "<") || strings.Contains(title, ">") {
+		return e.ErrTitleHasHTML
+	}
+
+	return nil
+}
+
+func ValidateContent(content string) error {
+	if strings.Contains(content, "<script") {
+		return e.ErrContentHasScript
+	}
+
+	return nil
+}
+
+func (a *Article) ChangeTitle(newTitle string) error {
+	if newTitle == a.Title {
+		return e.ErrArticleNameIsExists
+	}
+
+	a.Title = newTitle
+	return nil
+}
+
+func (a *Article) ChangeContent(newContent string) error {
+	if newContent == a.Content {
+		return e.ErrArticleContentIsExists
+	}
+
+	a.Content = newContent
+	return nil
+}
+
+func (a *Article) ChangeCategory(newCategory *Category) error {
+	if a.Category != nil && newCategory.ID == a.Category.ID {
+		return e.ErrCategoryIsExists
+	}
+
+	a.Category = newCategory
+	return nil
+}
+
+func (a *Article) CheckAuthor(userId uint) error {
+	if a.AuthorID != userId {
+		return e.ErrUserNotAuthor
+	}
+
+	return nil
+}
