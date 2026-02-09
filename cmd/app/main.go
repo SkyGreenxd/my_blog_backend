641d8ea024eab5a6aed7b0b751111eb6f51a1f08
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2026-02-09 23:26:47 +0300

    add swagger

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -4,6 +4,11 @@ import (
 	"my_blog_backend/internal/app"
 )
 
+// @title			My Blog API
+// @version		1.0
+// @description	API сервис для ведения блога.
+// @host			localhost:8080
+// @BasePath		/api/v1
 func main() {
 	app.Run()
 }
